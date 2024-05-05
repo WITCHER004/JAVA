33f12a58f147b32af0750c9b92dda05deb6999a8
Aryaman Saxena <135634505+WITCHER004@users.noreply.github.com>
2024-05-05 21:49:11 +0530

    Add files via upload
    This Java program prompts the user to input a number and then determines whether the input is an integer or not.

---
+++ det.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class det{
+    public static void main(String[]args){
+        System.out.println("HI,USER");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER NUMBER:");
+        Boolean a= sc.hasNextInt();
+        System.out.println(a);
+        sc.close();
+    }
+}
+
+        
+        
