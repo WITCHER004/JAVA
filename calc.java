709aeffbb7b3c527fde89361ca0d7a2bf1a11085
Aryaman Saxena <135634505+WITCHER004@users.noreply.github.com>
2024-05-06 23:36:17 +0530

    calc.java

--- calc.java
+++ calc.java
@@ -2,8 +2,12 @@ public class calc{
     public static void main(String[]args){
         int a = 25;
         int b = 25;
+        int mult= a*b;
+        int div= a/b;
         int sum=a+b;
         int diff=a-b;
+        System.out.println(mult);
+        System.out.println(div);
         System.out.println(diff);
         System.out.println(sum);
     }
