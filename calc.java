7a4d46c348936cdcdf141d840191302771500ca8
Aryaman Saxena <135634505+WITCHER004@users.noreply.github.com>
2024-05-06 23:34:45 +0530

    Add files via upload

---
+++ calc.java
@@ -0,0 +1,10 @@
+public class calc{
+    public static void main(String[]args){
+        int a = 25;
+        int b = 25;
+        int sum=a+b;
+        int diff=a-b;
+        System.out.println(diff);
+        System.out.println(sum);
+    }
+}
