7635ac6f25287a4a26a63438158c1717d4f8a553
Aryaman Saxena <135634505+WITCHER004@users.noreply.github.com>
2024-05-09 23:55:55 +0530

    Add files via upload

---
+++ LOOPS.java
@@ -0,0 +1,7 @@
+public class LOOPS{
+    public static void main(String[]args){
+        for(int  counter=0; counter<3;counter=counter+1){
+            System.out.println("HELLO WORLD:-");
+        }
+    }
+}
